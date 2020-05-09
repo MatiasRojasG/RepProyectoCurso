036d2c36044464f24b51d536a5f1ab1d1d7bc8a1
MatiasRojasG <62114178+MatiasRojasG@users.noreply.github.com>
2020-05-08 23:57:56 -0400

    Inicio repositorio

---
+++ ProyectodeCurso/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ProyectodeCurso
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
