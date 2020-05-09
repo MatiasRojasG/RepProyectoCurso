490e60f8fcf9a00e3b39023fc50e62973bb83d17
MatiasRojasG <62114178+MatiasRojasG@users.noreply.github.com>
2020-05-09 00:56:40 -0400

    Clases Listas, dudas con program

---
+++ ProyectodeCurso/PlaylistS.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ProyectodeCurso
+{
+    public class PlaylistS
+    {
+        //Atributos
+        private string OwnerUser;
+        private string Name_PlaylistS;
+        private bool PrivacyS; //Si vale true es privada, distinto publica
+
+
+        //Listas
+        public List<Profile> FollowersPS = new List<Profile> { };
+        public List<Songs> ListS = new List<Songs> { };
+
+
+        public string OwnerUser1 { get => OwnerUser; set => OwnerUser = value; }
+        public string Name_PlaylistS1 { get => Name_PlaylistS; set => Name_PlaylistS = value; }
+        public bool PrivacyS1 { get => PrivacyS; set => PrivacyS = value; }
+
+
+
+
+        public PlaylistS(string ownerser, string name_playlist, bool privacy)
+        {
+            this.OwnerUser1 = ownerser;
+            this.Name_PlaylistS1 = name_playlist;
+            this.PrivacyS1 = privacy;
+        }
+        //Añadir seguidor
+        public void AddFollowersPS(Profile profile)
+        {
+            FollowersPS.Add(profile);
+        }
+        //Añadir Cancion
+        public void Lists(Songs song)
+        {
+            ListS.Add(song);
+        }
+    }
+}
--- ProyectodeCurso/Program.cs
+++ ProyectodeCurso/Program.cs
@@ -1,12 +1,56 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
 
 namespace ProyectodeCurso
 {
     class MainClass
-    {
+    { 
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string Opcion1;
+            string nombrePlaylistS;
+            string privacyS;
+            //Si entra como usuario dueño de playlists
+            Console.WriteLine("Elija una opción:");
+            Console.WriteLine("1) Crear Playlist de Música");
+            Console.WriteLine("2) Ver Playlists de Música");
+            Console.WriteLine("3) Crear Playlist de Películas");
+            Console.WriteLine("4) Ver Playlists de Peliculas");
+            Opcion1 = Console.ReadLine();
+            if (Opcion1=="1")
+            {
+                Console.WriteLine("Ingrese el nombre de la Playlist");
+                nombrePlaylistS = Console.ReadLine();
+                Console.WriteLine("¿Desea que su Playlist sea privada?");
+                privacyS = Console.ReadLine();
+                if (privacyS == "Si")
+                {
+                    PlaylistS playlistSpriv = new PlaylistS("owneruser", nombrePlaylistS, true);
+
+                }
+                if (privacyS=="No")
+                {
+                    PlaylistS playlistSpub = new PlaylistS("owneruser", nombrePlaylistS,false);
+                }
+                else
+                {
+                    Console.WriteLine("Error / Favor elija una opción válida");
+                }
+            }
+
+
+
+
+            //Si es otro usuario
+
+
+
+
         }
     }
 }
