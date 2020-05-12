9c03f80212684c101bd1cef464144f51ab34971b
MatiasRojasG <62114178+MatiasRojasG@users.noreply.github.com>
2020-05-12 03:13:02 -0400

    Detalles

--- ProyectodeCurso/APP.cs
+++ ProyectodeCurso/APP.cs
@@ -11,7 +11,7 @@ namespace ProyectodeCurso
     {
         protected List<Songs> DataBaseSongs = new List<Songs>();
         protected List<PlaylistS> DataBasePlaylistS = new List<PlaylistS> { };
-
+        protected List<Songs> KeyWordSongs = new List<Songs> { };
 
         public APP()
         {
@@ -28,6 +28,50 @@ namespace ProyectodeCurso
                 return "Canción no encontrada";
             }
         }
+        public string SearchKeyWord(string Key, string type) //En teoria deberia entregar todas las canciones que tengan la coincidencia
+        {
+            if (type == "Música")
+            {
+                foreach (Songs song in DataBaseSongs)
+                {
+                    if (Key==song.Name || Key == song.Album || Key == song.Songgenre || Key == song.Singer || Key == song.Composer || Key == song.Yearpublishs
+                    || Key == song.TypefileS)
+                    {
+                        KeyWordSongs.Add(song); //Agrega las canciones que cumplan la coincidencia con el keyword
+                    }
+                }
+                foreach (Songs song1 in KeyWordSongs)
+                {
+                    return song1.InfoSong(); //Retorna la info de las canciones de la lista (las que coinciden)
+                }
+                KeyWordSongs.Clear(); //Se resetea la lista para una nueva busqueda
+            }
+            if (type=="Películas") //Lo mismo pero con los métodos de las películas
+            {
+                return "";
+            }
+            if (KeyWordSongs.Count==0) //Si la lista no contiene canciones ningun tuvo coincidencia con el keyword
+            {
+                return "No se encontraron coincidencias";
+            }
+            else
+            {
+                return "Error, porfavor ingrese una palabra clave válida";
+            }
+        }
+
+
+        //Agregar Cancion
+        public void AddSong(Songs song)
+        {
+            DataBaseSongs.Add(song);
+        }
+
+        //MB vi que ya están los métodos para guardar en la memoria y para verificar las canciones, asi que hago el resto de métodos
+
+
+
+
         //Busca una playlist
         public string SearchPlaylistS(PlaylistS playlist)
         {
@@ -43,6 +87,10 @@ namespace ProyectodeCurso
             {
                 return "Esta playlist no existe"; //Si no es ninguna de las dos, la playlist no existe
             }
+        }
+        public void AddPlaylistStoDataBase(PlaylistS playlist)
+        {
+            DataBasePlaylistS.Add(playlist);
         }
     }
 }
